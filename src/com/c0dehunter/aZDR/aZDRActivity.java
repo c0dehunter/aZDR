1549f35d2c24eb363da3837ee0735ccf66c60a0c
Primoz Kralj <primoz@codehunter.eu>
2012-02-29 11:49:45 +0100

    project creation

---
+++ src/com/c0dehunter/aZDR/aZDRActivity.java
@@ -0,0 +1,13 @@
+package com.c0dehunter.aZDR;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class aZDRActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
