f6c543927897aa57b7e49bf4091b42ef1891e2d9
Primoz Kralj <primoz@codehunter.eu>
2012-04-05 10:05:37 +0200

    Commit za prvi teden

--- src/com/c0dehunter/aZDR/aZDRActivity.java
+++ src/com/c0dehunter/aZDR/aZDRActivity.java
@@ -2,12 +2,15 @@ package com.c0dehunter.aZDR;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class aZDRActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        
     }
 }
